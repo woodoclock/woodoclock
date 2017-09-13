185fc5909e4e9259cb0aad1ed70514f71f04fb93
Daniil Pokinsokha <web.pokinsokha@gmail.com>
2017-09-14 04:21:48 +0700

    добавил два htaccess один в веб другой в корень. И добавил base url в веб конфиг

--- config/web.php
+++ config/web.php
@@ -11,6 +11,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'GNj1x-g3ilV4cJbrYW8fosnoZnZvIuY_',
+            'baseUrl' => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
