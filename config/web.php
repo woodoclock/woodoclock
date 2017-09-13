c6aa0721c7f1256e023463434459659ebc3348c3
Daniil Pokinsokha <web.pokinsokha@gmail.com>
2017-09-14 04:32:47 +0700

    удалил прошлые изменения

--- config/web.php
+++ config/web.php
@@ -11,7 +11,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'GNj1x-g3ilV4cJbrYW8fosnoZnZvIuY_',
-            'baseUrl' => '',
+            //'baseUrl' => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
